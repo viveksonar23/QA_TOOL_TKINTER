086d98eae6c85b5b9602b3c512139fb44420453a
nickumia <nickumia6@gmail.com>
2024-02-19 21:37:57 -0500

    lint: empty whitespace

--- main.py
+++ main.py
@@ -9,41 +9,41 @@ from qatool.Forget_Password import PasswordResetApp
 from qatool.Register import RegisterScreen
 
 class tkinterApp(tk.Tk):
-     
-    # __init__ function for class tkinterApp 
-    def __init__(self, *args, **kwargs): 
-         
+
+    # __init__ function for class tkinterApp
+    def __init__(self, *args, **kwargs):
+
         # __init__ function for class Tk
         tk.Tk.__init__(self, *args, **kwargs)
-         
+
         # creating a container
-        container = tk.Frame(self)  
+        container = tk.Frame(self)
         self.geometry('1132x611+300+100')
         self.configure(bg="#fff")
         self.resizable(True, True)
-        container.pack(side = "top", fill = "both", expand = True) 
-  
+        container.pack(side = "top", fill = "both", expand = True)
+
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0, weight = 1)
-  
+
         # initializing frames to an empty array
-        self.frames = {}  
-  
+        self.frames = {}
+
         # iterating through a tuple consisting
         # of the different page layouts
         for F in (RegisterScreen, PasswordResetApp, LoginScreen):
-  
+
             frame = F(container, self)
-  
+
             # initializing frame of that object from
-            # startpage, page1, page2 respectively with 
+            # startpage, page1, page2 respectively with
             # for loop
-            self.frames[F] = frame 
-  
+            self.frames[F] = frame
+
             frame.grid(row = 0, column = 0, sticky ="nsew")
-  
+
         self.show_frame(LoginScreen)
-  
+
     # to display the current frame passed as
     # parameter
     def show_frame(self, cont):
--- qatool/Register.py
+++ qatool/Register.py
@@ -16,7 +16,7 @@ root.configure(bg="#fff")
 root.resizable(True, True)
 img = PhotoImage(file=cwd+'/iconssss.png')
 root.iconphoto(False, img)
-     
+
 bg_img = PhotoImage(file=cwd+'/zoomimagepeopr.png')
 bg_label = Label(root, image=bg_img)
 bg_label.place(x=0, y=0, relwidth=1, relheight=1)
@@ -67,11 +67,11 @@ def register_page():
     else:
         con = mysql.connect(host="localhost", user ="root", password = "", database = "aut")
         cursor = con.cursor()
-       
+
         cursor.execute("INSERT INTO auth VALUES(%s, %s, %s, %s, %s, %s)", (first_name, last_name, username, password, security_question, security_answer))
         cursor.execute("commit")
         con.close()
-        
+
         root.destroy()
         show_message_box("Registration Successful", f"Registration Successful!\nWelcome, {first_name} {last_name}!")
         subprocess.Popen(["python3", "Login.py"], shell=False)
@@ -84,56 +84,56 @@ class RegisterScreen(tk.Frame):
         signup_frame = ttk.Frame(root, padding=10, relief='solid', borderwidth=1)
         signup_frame.place(x=400, y=150)
         signup_frame.pack(expand=True)
-        
+
         heading = Label(signup_frame, text='User Sign Up', font=('Arial', 20, 'bold'))
         heading.place(x=300, y=4)
         heading.grid(row=0, column=0, columnspan=5,padx=10, pady=10)
-        
+
         label_first_name = Label(signup_frame, text='First Name', font=('Microsoft YaHei UI Light', 15))
         label_first_name.grid(row=1, column=0, pady=5, sticky='e')
-        
+
         first_name_entry = Entry(signup_frame, width=30, font=('Microsoft YaHei UI Light', 15), bd=1, relief="solid", highlightthickness=0.5, highlightbackground="lightgray", highlightcolor="lightgray")
         first_name_entry.grid(row=1, column=1, pady=5, padx=5)
-        
+
         label_last_name = Label(signup_frame, text='Last Name', font=('Microsoft YaHei UI Light', 15))
         label_last_name.grid(row=2, column=0, pady=5, sticky='e')
-        
+
         last_name_entry = Entry(signup_frame, width=30, font=('Microsoft YaHei UI Light', 15), bd=1, relief="solid", highlightthickness=0.5, highlightbackground="lightgray", highlightcolor="lightgray")
         last_name_entry.grid(row=2, column=1, pady=5, padx=5)
-        
+
         label_username = Label(signup_frame, text='Create Username', font=('Microsoft YaHei UI Light', 15))
         label_username.grid(row=3, column=0, pady=5, sticky='e')
-        
+
         username_entry = Entry(signup_frame, width=30, font=('Microsoft YaHei UI Light', 15), bd=1, relief="solid", highlightthickness=0.5, highlightbackground="lightgray", highlightcolor="lightgray")
         username_entry.grid(row=3, column=1, pady=5, padx=5)
-        
+
         # Add "Security Question" combobox
         label_security_question = Label(signup_frame, text='Security Questions', font=('Microsoft YaHei UI Light', 15))
         label_security_question.grid(row=6, column=0, padx=5, pady=5, sticky='e')
-        
+
         security_questions = ["What is the name of your first car?", "Which Elementry school you attended?"]
         security_question_combobox = ttk.Combobox(signup_frame, values=security_questions, width=30, font=('Microsoft YaHei UI Light', 15), state="readonly")
         security_question_combobox.grid(row=6, column=1, pady=6, padx=6)
-        
+
         # Entry for security answer
         label_security_answer = Label(signup_frame, text='Security Answer', font=('Microsoft YaHei UI Light', 15))
         label_security_answer.grid(row=7, column=0, pady=5, sticky='e')
-        
+
         security_answer_entry = Entry(signup_frame, width=30, font=('Microsoft YaHei UI Light', 15), bd=1, relief="solid", highlightthickness=0.5, highlightbackground="lightgray", highlightcolor="lightgray")
         security_answer_entry.grid(row=7, column=1, pady=5, padx=5)
-        
+
         label_password = Label(signup_frame, text='Create Password', font=('Microsoft YaHei UI Light', 15))
         label_password.grid(row=4, column=0, pady=5, sticky='e')
-        
+
         password_entry = Entry(signup_frame, width=30, font=('Microsoft YaHei UI Light', 15), show='*', bd=1, relief="solid", highlightthickness=0.5, highlightbackground="lightgray", highlightcolor="lightgray")
         password_entry.grid(row=4, column=1, pady=5, padx=5)
-        
+
         label_confirm_password = Label(signup_frame, text='Confirm Password', font=('Microsoft YaHei UI Light', 15))
         label_confirm_password.grid(row=5, column=0, pady=5, sticky='e')
-        
+
         confirm_password_entry = Entry(signup_frame, width=30, font=('Microsoft YaHei UI Light', 15), show='*', bd=1, relief="solid", highlightthickness=0.5, highlightbackground="lightgray", highlightcolor="lightgray")
         confirm_password_entry.grid(row=5, column=1, pady=5, padx=5)
-        
+
         ttk.Style().configure("TButton", padding=6, relief="flat", background="gray", foreground="black", borderwidth=5, highlightbackground="black", highlightcolor="black", font=('Arial', 13, 'bold'))
         register_button = ttk.Button(signup_frame, text="Register", command=register_page, style="TButton")
         register_button.grid(row=8, column=1, pady=10)
